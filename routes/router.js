bb569491f278b4483826af99322459ba60ac8b9d
SamX23 <skalexsong@gmail.com>
2021-07-29 23:00:04 +0700

    Update : migrate dashboard route to controller

--- routes/router.js
+++ routes/router.js
@@ -8,7 +8,7 @@ const games = require("../controllers/gamesController");
 const login = require("../controllers/loginController");
 const register = require("../controllers/registerController");
 const dashboardUser = require("../controllers/dashboardUserController");
-const dashboard = require("./dashboard");
+const dashboard = require("../controllers/dashboardController");
 
 // GET METHOD
 router.get("/", home.index);
@@ -16,10 +16,16 @@ router.get("/games", games.index);
 router.get("/login", login.index);
 router.get("/login/auth", login.auth);
 router.get("/register", register.index);
+router.get("/dashboard", dashboard.index);
+router.get("/dashboard/*", dashboard.handler);
 router.get("/dashboard-user", dashboardUser.index);
 
 // POST METHOD
+router.post("/dashboard/add", dashboard.create);
+router.post("/dashboard/edit/:id", dashboard.update);
+router.post("/dashboard/delete/:id", dashboard.delete);
 router.post("/register/auth", register.register);
+// router.post("/dashboard-user", dashboardUser.update);
 
 router.use(dashboard);
 
