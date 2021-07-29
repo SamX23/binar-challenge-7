9fae765dab649fd01f155dc74880c4dabbb5002b
SamX23 <skalexsong@gmail.com>
2021-07-29 20:01:59 +0700

    Update : use head element as a partial

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use(function (err, req, res, next) {
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
   res.status(err.status || 500);
-  res.render("error");
+  res.render("error", { title: "Error Pages" });
 });
 
 module.exports = app;
