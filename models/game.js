9b6267158fbc59b66ed989b8cf49d88f648f84c8
SamX23 <skalexsong@gmail.com>
2021-08-05 05:12:47 +0700

    Update : add game table

---
+++ models/game.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Game extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Game.init({
+    player_one: DataTypes.STRING,
+    player_two: DataTypes.STRING,
+    result: DataTypes.STRING,
+    times: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Game',
+  });
+  return Game;
+};
