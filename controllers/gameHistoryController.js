e83047b8d5d9cb12666b9da5c7884b34590e0f0d
SamX23 <skalexsong@gmail.com>
2021-08-08 00:30:59 +0700

    Update : use latest ordering

--- controllers/gameHistoryController.js
+++ controllers/gameHistoryController.js
@@ -2,7 +2,7 @@ const { Game } = require("../models");
 module.exports = {
   index: async (req, res) =>
     await Game.findAll({
-      order: [["id", "ASC"]],
+      order: [["updatedAt", "DESC"]],
     }).then((game) =>
       res.status(200).render("gameHistory", {
         title: "Games History",
